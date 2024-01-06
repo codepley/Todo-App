b70b1ae4912cb698625de613b59d459a86a6214e
codepley <officialkushalkaran@gmail.com>
2024-01-06 12:07:50 +0530

    v1.0.0 complete

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,9 +1,34 @@
-export function CreateTodo() {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import axios from 'axios';
+
+export function CreateTodo({fetchData}) {
+
+   const [title, setTitle] = useState("");
+   const [description, setDescription] = useState("");
+
+   function handleSubmit() {
+      axios.post('http://localhost:3000/todos', {
+         title,
+         description
+       })
+       .then(function (response) {
+         fetchData();
+         setTitle("");
+         setDescription("");
+         console.log("done");
+         console.log(response);
+       })
+       .catch(function (error) {
+         console.log(error);
+       });
+   }
+
   return (
     <div>
-      <input type="text" placeholder="title" /> <br />
-      <input type="text" placeholder="description" /> <br />
-      <button>Add Todo</button>
+      <input type="text" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} /> <br />
+      <input type="text" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} /> <br />
+      <button onClick={handleSubmit}>Add Todo</button>
     </div>
   );
 }
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -2,16 +2,32 @@
 // import { useState } from "react"
 // import { Todo } from "./Todo";
 
-export function Todos({ todos }) {
-  // const [todos, setTodos] = useState([]);
+import axios from "axios";
+
+export function Todos({ todos, fetchData }) {
+   
+  function markComplete(id) {
+
+    axios.put('http://localhost:3000/completed', {id: id})
+    .then(function(response) {
+      console.log(response)
+      fetchData();
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+  }
+
   return (
     <div>
       {todos.map(function (todo) {
         return (
-          <div key={todo.id}>
-            <h1>{todo.title}</h1> 
+          <div key={todo._id}>
+            <h1>{todo.title}</h1>
             <h1>{todo.description}</h1>
-            <button>{todo.completed ? "Completed" : "Mark as complete"}</button>
+            <button onClick={() => markComplete(todo._id)}>
+              {todo.completed ? "Completed" : "Mark as complete"}
+            </button>
           </div>
         );
       })}
